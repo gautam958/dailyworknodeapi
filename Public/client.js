9b264eccd236c782d9b1fe6c14d016431bbcc190
Gautam Kumar <gautam958@gmail.com>
2021-04-23 00:30:01 +0800

    Update client.js
    add virtual path

--- Public/client.js
+++ Public/client.js
@@ -1,5 +1,5 @@
 $(function () {
-  $.get("/users", function (users) {
+  $.get("/dailyworknodeapi/users", function (users) {
     users.forEach(function (user) {
       $("<li></li>")
         .text(user.userid)
