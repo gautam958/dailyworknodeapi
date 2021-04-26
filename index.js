0689fe11cf430f4381b68a30f725b13a85cf7daf
Gautam Kumar <gautam958@gmail.com>
2021-04-26 17:04:25 +0800

    Add Cross Origin to *

--- index.js
+++ index.js
@@ -41,11 +41,11 @@ app.use(bodyParser.json());
 if (process.env.CORS) {
     app.use(cors());
 }
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Initialize routes middleware
 app.use('/api/users', require('./routes/users'));
